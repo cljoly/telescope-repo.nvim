8abebe853c0f3d2af05b941bd9be5300c9ee40c1
delphinus <me@delphinus.dev>
2021-01-05 10:38:15 +0900

    Add bootstrap

--- lua/telescope/_extensions/ghq.lua
+++ lua/telescope/_extensions/ghq.lua
@@ -1,40 +1,8 @@
-local conf = require('telescope.config').values
-local finders = require('telescope.finders')
-local pickers = require('telescope.pickers')
-local previewers = require('telescope.previewers')
+local builtin = require'telescope.builtin'
+local ghq_builtin = require'telescope._extensions.ghq_builtin'
 
-local M = {}
-
-M.ghq = function(opts)
-  opts = opts or {}
-  local cmd = vim.tbl_flatten{'ghq', 'list', '--full-path'}
-  pickers.new(opts, {
-    prompt_title = 'Repositories managed by ghq',
-    finder = finders.new_oneshot_job(
-      cmd,
-      opts
-    ),
-    previewer = previewers.new_termopen_previewer{
-      get_command = function(entry)
-        -- TODO: deal with another README
-        local readme
-        for _, name in pairs{'README', 'README.md'} do
-          local path = entry.value..'/'..name
-          local file_found = io.open(path)
-          if file_found then
-            io.close(file_found)
-            readme = path
-            break
-          end
-        end
-        if readme then
-          return {'bat', readme}
-        end
-        return {'echo', ''}
-      end,
-      sorter = conf.file_sorter(opts),
-    },
-  }):find()
-end
-
-return M
+return require'telescope'.register_extension{
+  setup = function()
+    builtin.ghq_list = ghq_builtin.list
+  end,
+}
