5b07b3cabd5ebf6c5c7b3f24476446f4fcc4c2ed
delphinus <me@delphinus.dev>
2021-01-13 09:21:37 +0900

    Use a robust way to detect README filenames

--- lua/telescope/_extensions/ghq_builtin.lua
+++ lua/telescope/_extensions/ghq_builtin.lua
@@ -2,6 +2,7 @@ local actions = require'telescope.actions'
 local conf = require'telescope.config'.values
 local finders = require'telescope.finders'
 local from_entry = require'telescope.from_entry'
+local path = require'telescope.path'
 local pickers = require'telescope.pickers'
 local previewers = require'telescope.previewers'
 
@@ -20,11 +21,11 @@ M.list = function(opts)
         -- TODO: deal with other README's
         local readme
         for _, name in pairs{'README', 'README.md', 'README.markdown'} do
-          local path = entry.value..'/'..name
-          local file_found = io.open(path)
+          local filepath = entry.value..path.separator..name
+          local file_found = io.open(filepath)
           if file_found then
             io.close(file_found)
-            readme = path
+            readme = filepath
             break
           end
         end
@@ -44,15 +45,15 @@ M.list = function(opts)
       actions._goto_file_selection:replace(function(_, cmd)
         local entry = actions.get_selected_entry()
         actions.close(prompt_bufnr)
-        local path = from_entry.path(entry)
+        local dir = from_entry.path(entry)
         if cmd == 'edit' then
-          require'telescope.builtin'.git_files{cwd = path}
+          require'telescope.builtin'.git_files{cwd = dir}
         elseif cmd == 'new' then
-          vim.cmd('cd '..path)
-          print('chdir to '..path)
+          vim.cmd('cd '..dir)
+          print('chdir to '..dir)
         elseif cmd == 'vnew' then
-          vim.cmd('lcd '..path)
-          print('lchdir to '..path)
+          vim.cmd('lcd '..dir)
+          print('lchdir to '..dir)
         end
       end)
       return true
