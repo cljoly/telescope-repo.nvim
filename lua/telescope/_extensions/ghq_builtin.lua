3cad28f5703b77ddaaf1083ac0e78755d45c8abf
delphinus <me@delphinus.dev>
2021-01-13 10:26:58 +0900

    Detect doc in addition to README

--- lua/telescope/_extensions/ghq_builtin.lua
+++ lua/telescope/_extensions/ghq_builtin.lua
@@ -8,6 +8,36 @@ local previewers = require'telescope.previewers'
 
 local M = {}
 
+local function is_readable(filepath)
+  local fd = vim.loop.fs_open(filepath, 'r', 438)
+  local result = fd and true or false
+  if result then
+    vim.loop.fs_close(fd)
+  end
+  return result
+end
+
+local function search_readme(dir)
+  for _, name in pairs{'README', 'README.md', 'README.markdown'} do
+    local filepath = dir..path.separator..name
+    if is_readable(filepath) then
+      return filepath
+    end
+  end
+  return nil
+end
+
+local function search_doc(dir)
+  local doc_path = vim.fn.join({dir, 'doc', '**', '*.txt'}, path.separator)
+  local maybe_doc = vim.split(vim.fn.glob(doc_path), '\n')
+  for _, filepath in pairs(maybe_doc) do
+    if is_readable(filepath) then
+      return filepath
+    end
+  end
+  return nil
+end
+
 M.list = function(opts)
   opts = opts or {}
   pickers.new(opts, {
@@ -18,24 +48,22 @@ M.list = function(opts)
     ),
     previewer = previewers.new_termopen_previewer{
       get_command = function(entry)
-        -- TODO: deal with other README's
-        local readme
-        for _, name in pairs{'README', 'README.md', 'README.markdown'} do
-          local filepath = entry.value..path.separator..name
-          local file_found = io.open(filepath)
-          if file_found then
-            io.close(file_found)
-            readme = filepath
-            break
-          end
+        local dir = from_entry.path(entry)
+        local doc = search_readme(dir)
+        local is_mardown
+        if doc then
+          is_mardown = true
+        else
+          -- TODO: doc may be previewed in a plain text. Can I use syntax highlight?
+          doc = search_doc(dir)
         end
-        if readme then
-          if vim.fn.executable'glow' == 1 then
-            return {'glow', readme}
+        if doc then
+          if is_mardown and vim.fn.executable'glow' == 1 then
+            return {'glow', doc}
           elseif vim.fn.executable'bat' == 1 then
-            return {'bat', '--style', 'header,grid', readme}
+            return {'bat', '--style', 'header,grid', doc}
           end
-          return {'cat', readme}
+          return {'cat', doc}
         end
         return {'echo', ''}
       end,
