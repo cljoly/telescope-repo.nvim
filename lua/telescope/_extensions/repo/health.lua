00f84baca7d9f363c90429a4391f2c70c74c1bb9
Clément Joly <l@131719.xyz>
2021-12-16 21:39:05 +0000

    fix: address non local variable, unused imports…

--- lua/telescope/_extensions/repo/health.lua
+++ lua/telescope/_extensions/repo/health.lua
@@ -1,6 +1,5 @@
 local M = {}
 
-local main = require("telescope._extensions.repo.main")
 local utils = require("telescope._extensions.repo.utils")
 local list = require'telescope._extensions.repo.list'
 local cached_list = require'telescope._extensions.repo.cached_list'
@@ -33,7 +32,7 @@ local function find_repos(opts, cmd_with_args, telescope_cmd)
 end
 
 local function check_list_cmd()
-	fd_bin = utils.find_fd_binary()
+	local fd_bin = utils.find_fd_binary()
 	if fd_bin ~= "" then
 		health.report_ok("fd: found `" .. fd_bin .. "`\n" .. get_version(fd_bin))
 
@@ -46,7 +45,7 @@ local function check_list_cmd()
 end
 
 local function check_cached_list_cmd()
-	locate_bin = utils.find_locate_binary()
+	local locate_bin = utils.find_locate_binary()
 	if locate_bin ~= "" then
 		health.report_ok("locate: found `" .. locate_bin .. "`\n" .. get_version(locate_bin))
 
@@ -60,13 +59,13 @@ local function check_cached_list_cmd()
 end
 
 local function check_previewer()
-	markdown_bin = utils.find_markdown_previewer_for_document("test_doc.md")
+	local markdown_bin = utils.find_markdown_previewer_for_document("test_doc.md")
 	if markdown_bin[1] ~= utils._markdown_previewer[1][1] then
 		health.report_warn("Install `" .. utils._markdown_previewer[1][1] .. "` for a better preview of markdown files")
 	end
 	health.report_ok("Will use `" .. markdown_bin[1] .. "` to preview markdown READMEs")
 
-	generic_bin = utils.find_generic_previewer_for_document("test_doc")
+	local generic_bin = utils.find_generic_previewer_for_document("test_doc")
 	if generic_bin[1] ~= utils._generic_previewer[1][1] then
 		health.report_warn(
 			"Install `" .. utils._generic_previewer[1][1] .. "` for a better preview of non-markdown files"
