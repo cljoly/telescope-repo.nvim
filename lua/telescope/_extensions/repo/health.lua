6a42d94d8c9252c1f0f5710c3a22550dd865d9a4
Dan Sully <dan+github@sully.org>
2022-01-31 16:57:18 -0800

    Add stylua.toml & format.

--- lua/telescope/_extensions/repo/health.lua
+++ lua/telescope/_extensions/repo/health.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 local utils = require("telescope._extensions.repo.utils")
-local list = require'telescope._extensions.repo.list'
-local cached_list = require'telescope._extensions.repo.cached_list'
+local list = require("telescope._extensions.repo.list")
+local cached_list = require("telescope._extensions.repo.cached_list")
 
 local health = require("health")
 
@@ -10,83 +10,93 @@ local Job = require("plenary.job")
 local max_repo = 2
 
 local function get_version(binary)
-	local handle = io.popen(binary .. " --version")
-	local version = handle:read("*a")
-	handle:close()
-	return version
+    local handle = io.popen(binary .. " --version")
+    local version = handle:read("*a")
+    handle:close()
+    return version
 end
 
 local function find_repos(opts, cmd_with_args, telescope_cmd)
-		local j = Job:new({
-			command = cmd_with_args[1],
-			args = vim.list_slice(cmd_with_args, 2, #cmd_with_args),
-		})
-		j:sync()
+    local j = Job:new({
+        command = cmd_with_args[1],
+        args = vim.list_slice(cmd_with_args, 2, #cmd_with_args),
+    })
+    j:sync()
 
-		if j.code == 0 then
-			local some_repos = vim.list_slice(j:result(), 1, max_repo)
-			health.report_info("Repos found for `"..telescope_cmd.."`:\n"..table.concat(some_repos, ", ").."...")
-		else
-			health.report_error("`"..telescope_cmd.."` was unsuccessful. Exit code: "..tostring(j.code).."\n"..j.command.." "..table.concat(j.args, " ").."\n "..table.concat(j:result(), "\n")..table.concat(j:stderr_result(), "\n"))
-		end
+    if j.code == 0 then
+        local some_repos = vim.list_slice(j:result(), 1, max_repo)
+        health.report_info("Repos found for `" .. telescope_cmd .. "`:\n" .. table.concat(some_repos, ", ") .. "...")
+    else
+        health.report_error(
+            "`"
+                .. telescope_cmd
+                .. "` was unsuccessful. Exit code: "
+                .. tostring(j.code)
+                .. "\n"
+                .. j.command
+                .. " "
+                .. table.concat(j.args, " ")
+                .. "\n "
+                .. table.concat(j:result(), "\n")
+                .. table.concat(j:stderr_result(), "\n")
+        )
+    end
 end
 
 local function check_list_cmd()
-	local fd_bin = utils.find_fd_binary()
-	if fd_bin ~= "" then
-		health.report_ok("fd: found `" .. fd_bin .. "`\n" .. get_version(fd_bin))
+    local fd_bin = utils.find_fd_binary()
+    if fd_bin ~= "" then
+        health.report_ok("fd: found `" .. fd_bin .. "`\n" .. get_version(fd_bin))
 
-		local opts = {}
-		local command_with_args = list.prepare_command(opts)
-		find_repos(opts, command_with_args, ':Telescope repo list')
-	else
-		health.report_error("`list` will not function without [fd](https://github.com/sharkdp/fd)")
-	end
+        local opts = {}
+        local command_with_args = list.prepare_command(opts)
+        find_repos(opts, command_with_args, ":Telescope repo list")
+    else
+        health.report_error("`list` will not function without [fd](https://github.com/sharkdp/fd)")
+    end
 end
 
 local function check_cached_list_cmd()
-	local locate_bin = utils.find_locate_binary()
-	if locate_bin ~= "" then
-		health.report_ok("locate: found `" .. locate_bin .. "`\n" .. get_version(locate_bin))
+    local locate_bin = utils.find_locate_binary()
+    if locate_bin ~= "" then
+        health.report_ok("locate: found `" .. locate_bin .. "`\n" .. get_version(locate_bin))
 
-		local opts = {}
+        local opts = {}
 
-		local command_with_args = cached_list.prepare_command(opts)
+        local command_with_args = cached_list.prepare_command(opts)
 
-                -- lolcate doesn't have limit functionality.
-                if locate_bin == "lolcate" then
-		    command_with_args = {locate_bin, '--info'}
-                else
-		    command_with_args = vim.tbl_flatten({command_with_args, {'-l', tostring(max_repo)}})
-                end
+        -- lolcate doesn't have limit functionality.
+        if locate_bin == "lolcate" then
+            command_with_args = { locate_bin, "--info" }
+        else
+            command_with_args = vim.tbl_flatten({ command_with_args, { "-l", tostring(max_repo) } })
+        end
 
-		find_repos(opts, command_with_args, ':Telescope repo cached_list')
-	else
-		health.report_error("`cached_list` will not function without locate")
-	end
+        find_repos(opts, command_with_args, ":Telescope repo cached_list")
+    else
+        health.report_error("`cached_list` will not function without locate")
+    end
 end
 
 local function check_previewer()
-	local markdown_bin = utils.find_markdown_previewer_for_document("test_doc.md")
-	if markdown_bin[1] ~= utils._markdown_previewer[1][1] then
-		health.report_warn("Install `" .. utils._markdown_previewer[1][1] .. "` for a better preview of markdown files")
-	end
-	health.report_ok("Will use `" .. markdown_bin[1] .. "` to preview markdown READMEs")
+    local markdown_bin = utils.find_markdown_previewer_for_document("test_doc.md")
+    if markdown_bin[1] ~= utils._markdown_previewer[1][1] then
+        health.report_warn("Install `" .. utils._markdown_previewer[1][1] .. "` for a better preview of markdown files")
+    end
+    health.report_ok("Will use `" .. markdown_bin[1] .. "` to preview markdown READMEs")
 
-	local generic_bin = utils.find_generic_previewer_for_document("test_doc")
-	if generic_bin[1] ~= utils._generic_previewer[1][1] then
-		health.report_warn(
-			"Install `" .. utils._generic_previewer[1][1] .. "` for a better preview of non-markdown files"
-		)
-	end
-	health.report_ok("Will use `" .. generic_bin[1] .. "` to preview non-markdown READMEs")
+    local generic_bin = utils.find_generic_previewer_for_document("test_doc")
+    if generic_bin[1] ~= utils._generic_previewer[1][1] then
+        health.report_warn("Install `" .. utils._generic_previewer[1][1] .. "` for a better preview of non-markdown files")
+    end
+    health.report_ok("Will use `" .. generic_bin[1] .. "` to preview non-markdown READMEs")
 end
 
 M.check = function()
-	-- Ordered from fastest to slowest
-	check_previewer()
-	check_cached_list_cmd()
-	check_list_cmd()
+    -- Ordered from fastest to slowest
+    check_previewer()
+    check_cached_list_cmd()
+    check_list_cmd()
 end
 
 return M
--- lua/telescope/_extensions/repo/main.lua
+++ lua/telescope/_extensions/repo/main.lua
@@ -1,189 +1,203 @@
 local os_home = vim.loop.os_homedir()
 -- External dependancies
-local actions = require'telescope.actions'
-local actions_set = require'telescope.actions.set'
-local actions_state = require'telescope.actions.state'
-local conf = require'telescope.config'.values
-local entry_display = require'telescope.pickers.entry_display'
-local finders = require'telescope.finders'
-local from_entry = require'telescope.from_entry'
-local log = require "telescope.log"
-local pickers = require'telescope.pickers'
-local previewers = require'telescope.previewers'
-local t_utils = require'telescope.utils'
-local Path = require'plenary.path'
+local actions = require("telescope.actions")
+local actions_set = require("telescope.actions.set")
+local actions_state = require("telescope.actions.state")
+local conf = require("telescope.config").values
+local entry_display = require("telescope.pickers.entry_display")
+local finders = require("telescope.finders")
+local from_entry = require("telescope.from_entry")
+local log = require("telescope.log")
+local pickers = require("telescope.pickers")
+local previewers = require("telescope.previewers")
+local t_utils = require("telescope.utils")
+local Path = require("plenary.path")
 
 -- Other modules in this plugin
-local utils = require'telescope._extensions.repo.utils'
-local list = require'telescope._extensions.repo.list'
-local cached_list = require'telescope._extensions.repo.cached_list'
+local utils = require("telescope._extensions.repo.utils")
+local list = require("telescope._extensions.repo.list")
+local cached_list = require("telescope._extensions.repo.cached_list")
 
 local M = {}
 
 local function search_markdown_readme(dir)
-  for _, name in pairs{
-    'README', 'README.md', 'README.markdown', 'README.mkd',
-  } do
-    local file = dir / name
-    if file:is_file() then return file end
-  end
-  return nil
+    for _, name in
+        pairs({
+            "README",
+            "README.md",
+            "README.markdown",
+            "README.mkd",
+        })
+    do
+        local file = dir / name
+        if file:is_file() then
+            return file
+        end
+    end
+    return nil
 end
 
 local function search_generic_readme(dir)
-  local doc_path = Path:new(dir, 'README.*')
-  local maybe_doc = vim.split(vim.fn.glob(doc_path.filename), '\n')
-  for _, filepath in pairs(maybe_doc) do
-    local file = Path:new(filepath)
-    if file:is_file() then return file end
-  end
-  return nil
+    local doc_path = Path:new(dir, "README.*")
+    local maybe_doc = vim.split(vim.fn.glob(doc_path.filename), "\n")
+    for _, filepath in pairs(maybe_doc) do
+        local file = Path:new(filepath)
+        if file:is_file() then
+            return file
+        end
+    end
+    return nil
 end
 
 local function search_doc(dir)
-  local doc_path = Path:new(dir, 'doc', '**', '*.txt')
-  local maybe_doc = vim.split(vim.fn.glob(doc_path.filename), '\n')
-  for _, filepath in pairs(maybe_doc) do
-    local file = Path:new(filepath)
-    if file:is_file() then return file end
-  end
-  return nil
+    local doc_path = Path:new(dir, "doc", "**", "*.txt")
+    local maybe_doc = vim.split(vim.fn.glob(doc_path.filename), "\n")
+    for _, filepath in pairs(maybe_doc) do
+        local file = Path:new(filepath)
+        if file:is_file() then
+            return file
+        end
+    end
+    return nil
 end
 
 -- Was gen_from_ghq in telescope-ghq.nvim
 local function gen_from_fd(opts)
-  local displayer = entry_display.create{
-    items = {{}},
-  }
+    local displayer = entry_display.create({
+        items = { {} },
+    })
 
-  local function make_display(entry)
-    local dir = (function(path)
-      if path == Path.path.root() then return path end
+    local function make_display(entry)
+        local dir = (function(path)
+            if path == Path.path.root() then
+                return path
+            end
 
-      local p = Path:new(path)
-      if opts.tail_path then
-        local parts = p:_split()
-        return parts[#parts]
-      end
+            local p = Path:new(path)
+            if opts.tail_path then
+                local parts = p:_split()
+                return parts[#parts]
+            end
 
-      if opts.shorten_path then return p:shorten() end
+            if opts.shorten_path then
+                return p:shorten()
+            end
 
-      if vim.startswith(path, opts.cwd) and path ~= opts.cwd then
-        return Path:new(p):make_relative(opts.cwd)
-      end
+            if vim.startswith(path, opts.cwd) and path ~= opts.cwd then
+                return Path:new(p):make_relative(opts.cwd)
+            end
 
-      if vim.startswith(path, os_home) then
-        return (Path:new'~' / p:make_relative(os_home)).filename
-      end
-      return path
-    end)(entry.path)
+            if vim.startswith(path, os_home) then
+                return (Path:new("~") / p:make_relative(os_home)).filename
+            end
+            return path
+        end)(entry.path)
 
-    return displayer{dir}
-  end
+        return displayer({ dir })
+    end
 
-  return function(line)
-    return {
-      value = line,
-      ordinal = line,
-      path = line,
-      display = make_display,
-    }
-  end
+    return function(line)
+        return {
+            value = line,
+            ordinal = line,
+            path = line,
+            display = make_display,
+        }
+    end
 end
 
 -- Wrap entries to remove the part we used to detect the VCS. For instance, for git:
 -- - we get entries like “/home/me/repo/.git”
 -- - we want to send entries like “/home/me/repo”
 local function gen_from_locate_wrapper(opts)
-  log.trace "Called gen_from_locate_wrapper"
-  -- TODO Make this a wrapper over any function, not just gen_from_fd
-  -- TODO It’s not great for performance to parse paths in the whole list like this
-  return function(line_with_dotgit)
-    log.trace("line_with_dotgit " .. line_with_dotgit)
-    local line = Path:new(line_with_dotgit):parent().filename
-    return gen_from_fd(opts)(line)
-  end
+    log.trace("Called gen_from_locate_wrapper")
+    -- TODO Make this a wrapper over any function, not just gen_from_fd
+    -- TODO It’s not great for performance to parse paths in the whole list like this
+    return function(line_with_dotgit)
+        log.trace("line_with_dotgit " .. line_with_dotgit)
+        local line = Path:new(line_with_dotgit):parent().filename
+        return gen_from_fd(opts)(line)
+    end
 end
 
 local function project_files(opts)
-  local ok = pcall(require'telescope.builtin'.git_files, opts)
-  if not ok then require'telescope.builtin'.find_files(opts) end
+    local ok = pcall(require("telescope.builtin").git_files, opts)
+    if not ok then
+        require("telescope.builtin").find_files(opts)
+    end
 end
 
 local function project_live_grep(opts)
-  require'telescope.builtin'.live_grep(opts)
+    require("telescope.builtin").live_grep(opts)
 end
 
 local function call_picker(opts, command, prompt_title_supplement)
-  local prompt_title = 'Git repositories'
-  if prompt_title_supplement ~= nil then
-    prompt_title = prompt_title .. prompt_title_supplement
-  end
-  pickers.new(opts, {
-    prompt_title = prompt_title,
-    finder = finders.new_oneshot_job(
-      command,
-      opts
-    ),
-    previewer = previewers.new_termopen_previewer{
-      get_command = function(entry)
-        local dir = Path:new(from_entry.path(entry))
-        local doc = search_markdown_readme(dir)
-        if doc then
-          return utils.find_markdown_previewer_for_document(doc.filename)
-        end
-        doc = search_generic_readme(dir)
-        if not doc then
-          -- TODO: doc may be previewed in a plain text. Can I use syntax highlight?
-          doc = search_doc(dir)
-        end
-        if not doc then
-          return {'echo', ''}
-        end
-        return utils.find_generic_previewer_for_document(doc.filename)
-      end,
-    },
-    sorter = conf.file_sorter(opts),
-    attach_mappings = function(prompt_bufnr)
-      actions_set.select:replace(function(_, type)
-        local entry = actions_state.get_selected_entry()
-        local dir = from_entry.path(entry)
-        if type == 'default' then
-          project_files{cwd = dir}
-          return
-        end
-        if type == 'vertical' then
-          project_live_grep{cwd = dir}
-          return
-        end
-        if type == 'tab' then
-          vim.cmd("tabe "..dir)
-          vim.cmd("tcd "..dir)
-          project_files{cwd = dir}
-          return
-        end
-      end)
-      return true
-    end,
-  }):find()
+    local prompt_title = "Git repositories"
+    if prompt_title_supplement ~= nil then
+        prompt_title = prompt_title .. prompt_title_supplement
+    end
+    pickers.new(opts, {
+        prompt_title = prompt_title,
+        finder = finders.new_oneshot_job(command, opts),
+        previewer = previewers.new_termopen_previewer({
+            get_command = function(entry)
+                local dir = Path:new(from_entry.path(entry))
+                local doc = search_markdown_readme(dir)
+                if doc then
+                    return utils.find_markdown_previewer_for_document(doc.filename)
+                end
+                doc = search_generic_readme(dir)
+                if not doc then
+                    -- TODO: doc may be previewed in a plain text. Can I use syntax highlight?
+                    doc = search_doc(dir)
+                end
+                if not doc then
+                    return { "echo", "" }
+                end
+                return utils.find_generic_previewer_for_document(doc.filename)
+            end,
+        }),
+        sorter = conf.file_sorter(opts),
+        attach_mappings = function(prompt_bufnr)
+            actions_set.select:replace(function(_, type)
+                local entry = actions_state.get_selected_entry()
+                local dir = from_entry.path(entry)
+                if type == "default" then
+                    project_files({ cwd = dir })
+                    return
+                end
+                if type == "vertical" then
+                    project_live_grep({ cwd = dir })
+                    return
+                end
+                if type == "tab" then
+                    vim.cmd("tabe " .. dir)
+                    vim.cmd("tcd " .. dir)
+                    project_files({ cwd = dir })
+                    return
+                end
+            end)
+            return true
+        end,
+    }):find()
 end
 
 -- List of repos built using locate (or variants)
 M.cached_list = function(opts)
-  opts = opts or {}
-  opts.entry_maker = t_utils.get_lazy_default(opts.entry_maker, gen_from_locate_wrapper, opts)
-  local locate_command = cached_list.prepare_command(opts)
+    opts = opts or {}
+    opts.entry_maker = t_utils.get_lazy_default(opts.entry_maker, gen_from_locate_wrapper, opts)
+    local locate_command = cached_list.prepare_command(opts)
 
-  call_picker(opts, locate_command, ' (cached)')
+    call_picker(opts, locate_command, " (cached)")
 end
 
 -- Always up to date list of repos built using fd
 M.list = function(opts)
-  opts = opts or {}
-  opts.entry_maker = t_utils.get_lazy_default(opts.entry_maker, gen_from_fd, opts)
-  local fd_command = list.prepare_command(opts)
+    opts = opts or {}
+    opts.entry_maker = t_utils.get_lazy_default(opts.entry_maker, gen_from_fd, opts)
+    local fd_command = list.prepare_command(opts)
 
-  call_picker(opts, fd_command, ' (built on the fly)')
+    call_picker(opts, fd_command, " (built on the fly)")
 end
 
 return M
