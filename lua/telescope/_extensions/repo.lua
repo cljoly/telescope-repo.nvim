9be0b80a228b8170dcdfa981ef32e7f56e0b3569
Dan Sully <dan+github@sully.org>
2022-01-31 16:57:18 -0800

    Add stylua.toml & format.

--- lua/telescope/_extensions/repo.lua
+++ lua/telescope/_extensions/repo.lua
@@ -1,15 +1,15 @@
-local main = require'telescope._extensions.repo.main'
-local health = require'telescope._extensions.repo.health'
+local main = require("telescope._extensions.repo.main")
+local health = require("telescope._extensions.repo.health")
 
-return require'telescope'.register_extension{
-  health = health.check,
-  exports = {
-    list = main.list,
-    cached_list = main.cached_list,
-    -- Default command, for now, may change
-    repo = function (opts)
-      vim.api.nvim_echo({{"Falling back to `:Telescope repo list`, but this behavior may change in the future"}}, true, {})
-      main.list(opts)
-    end,
-  },
-}
+return require("telescope").register_extension({
+    health = health.check,
+    exports = {
+        list = main.list,
+        cached_list = main.cached_list,
+        -- Default command, for now, may change
+        repo = function(opts)
+            vim.api.nvim_echo({ { "Falling back to `:Telescope repo list`, but this behavior may change in the future" } }, true, {})
+            main.list(opts)
+        end,
+    },
+})
--- lua/telescope/_extensions/repo/cached_list.lua
+++ lua/telescope/_extensions/repo/cached_list.lua
@@ -1,32 +1,32 @@
 local M = {}
 
-local log = require "telescope.log"
-local utils = require'telescope._extensions.repo.utils'
+local log = require("telescope.log")
+local utils = require("telescope._extensions.repo.utils")
 
 -- Prepare locate command and change opts accordingly
 M.prepare_command = function(opts)
-	opts = opts or {}
-  opts.bin = opts.bin or utils.find_locate_binary()
-  if opts.bin == "" then
-    error "Please install locate (or one of its alternatives)"
-  end
-  opts.cwd = vim.env.HOME
+    opts = opts or {}
+    opts.bin = opts.bin or utils.find_locate_binary()
+    if opts.bin == "" then
+        error("Please install locate (or one of its alternatives)")
+    end
+    opts.cwd = vim.env.HOME
 
-  local repo_pattern = opts.pattern or [[/\.git$]] -- We match on the whole path
-  local locate_opts = opts.locate_opts or {}
+    local repo_pattern = opts.pattern or [[/\.git$]] -- We match on the whole path
+    local locate_opts = opts.locate_opts or {}
 
-  local locate_arg = '-r'
+    local locate_arg = "-r"
 
-  -- lolcate uses -b for pattern/regex match.
-  if opts.bin == "lolcate" then
-    locate_arg = '-b'
-  end
+    -- lolcate uses -b for pattern/regex match.
+    if opts.bin == "lolcate" then
+        locate_arg = "-b"
+    end
 
-  local locate_command = vim.tbl_flatten{opts.bin, locate_opts, locate_arg, repo_pattern}
+    local locate_command = vim.tbl_flatten({ opts.bin, locate_opts, locate_arg, repo_pattern })
 
-  log.trace("locate_command: "..vim.inspect(locate_command))
+    log.trace("locate_command: " .. vim.inspect(locate_command))
 
-  return locate_command
+    return locate_command
 end
 
 return M
