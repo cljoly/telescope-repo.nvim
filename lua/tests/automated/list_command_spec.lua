067d88296ae456fe21826dce9ff2ab5f45c928be
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-03-13 23:32:48 -0600

    fix(tests): bad `Makefile` syntax, tests improved
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/tests/automated/list_command_spec.lua
+++ lua/tests/automated/list_command_spec.lua
@@ -1,13 +1,13 @@
 describe("integration tests: ", function()
     it("Telescope repo repo", function()
-        vim.cmd([[Telescope repo repo]])
+        vim.cmd.Telescope({ args = { "repo", "repo" } })
     end)
 
     it("Telescope repo list", function()
-        vim.cmd([[Telescope repo list]])
+        vim.cmd.Telescope({ args = { "repo", "list" } })
     end)
 
     it("Telescope repo cached_list", function()
-        vim.cmd([[Telescope repo cached_list]])
+        vim.cmd.Telescope({ args = { "repo", "cached_list" } })
     end)
 end)
